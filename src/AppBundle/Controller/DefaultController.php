c9afe7c8de4fdfe9f6d804c345720af73175c35d
bouzilaila <laila86@hotmail.fr>
2018-10-08 22:54:22 +0200

    test

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -29,7 +29,7 @@ class DefaultController extends Controller
         
         return $this->render('@App/evenements/evenement.html.twig', [
             'base_dir' => realpath($this->getParameter('kernel.project_dir')).DIRECTORY_SEPARATOR,
-            'events' => $this->getEvents($e->latitude,$e->longitude) 
+            'events' => $this->getEvents($latitude,$longitude) 
         ]);
     }
 
