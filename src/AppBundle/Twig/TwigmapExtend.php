37fc542f6d943dd12a9453d417414a510b3a0fb1
bouzilaila <laila86@hotmail.fr>
2018-10-11 16:28:42 +0200

    test

--- src/AppBundle/Twig/TwigmapExtend.php
+++ src/AppBundle/Twig/TwigmapExtend.php
@@ -7,6 +7,8 @@ use Twig\TwigFunction;
 
 class TwigmapExtend extends AbstractExtension
 {
+
+    // map
     public function getFunctions()
     {
         return array(
@@ -19,5 +21,26 @@ class TwigmapExtend extends AbstractExtension
         $mapLeaflet = "<div id='$map'></div>";
     
         return $mapLeaflet;
+    }
+
+    //icones 
+
+    public function getIconeFunctions()
+    {
+        return array(
+            new TwigFunction('leaflet_icone', array($this, 'leafletIconeFunction')),
+        );
+    }
+
+    public function leafletIconeFunction()
+    {
+        $events = $this -> getEvents();
+
+        foreach($events as $e)
+        {
+            $marker = display_marker($events);
+        }
+
+        return $marker;
     }
 }
