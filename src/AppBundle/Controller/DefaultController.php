3421dec624c42b97bdf981450e983cb039cf64f3
bouzilaila <laila86@hotmail.fr>
2018-10-15 15:19:59 +0200

    test

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -67,7 +67,8 @@ class DefaultController extends Controller
             'base_dir' => realpath($this->getParameter('kernel.project_dir')).DIRECTORY_SEPARATOR,
             'events' => $gpsEvents,
             'latitude' => $gps[1],
-            'longitude' => $gps[0]
+            'longitude' => $gps[0],
+            'map' => $map
 
             // 'latitude' => $e->$gps[1],
             // 'longitude' => $e->$gps[0]
