8969e84384e75a87aa214dc5dbc09bfc4db39245
bouzilaila <laila86@hotmail.fr>
2018-10-18 12:11:28 +0200

    test

--- src/AppBundle/Controller/EventsController.php
+++ src/AppBundle/Controller/EventsController.php
@@ -48,7 +48,7 @@ class EventsController extends Controller
 
             }
 
-            var_dump(json_encode($gpsEvents)); die;
+            // var_dump(json_encode($gpsEvents)); die;
 
         return $this->json($gpsEvents);
     }
