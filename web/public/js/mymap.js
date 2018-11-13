72237c7e8e83242dd57d8af6b5d6eb23b897d24f
bouzilaila <laila86@hotmail.fr>
2018-11-13 17:24:07 +0100

    creat fonctiopn adventlistener

--- web/public/js/mymap.js
+++ web/public/js/mymap.js
@@ -27,9 +27,11 @@ function display_marker(mymap,latitude,longitude,nom,adresse)
     .openPopup();
 }
 
-function get_markers(e)
-{
-    eventType = e.target.id // recupere id du bouton un evenement se produit 
+function get_markers(e = null)
+{   
+    // Si la variable est null alors afficher tout les evenements sinon afficher en fonction du clique choisi
+    eventType = e === null ? 'allEvent' : e.target.id;// recupere id du bouton un evenement se produit 
+
     fetch(`http://51.75.31.43/dossierexotest2/web/app_dev.php/evenements/json?option=${eventType}`)
     .then(function(response){
         return response.json();
